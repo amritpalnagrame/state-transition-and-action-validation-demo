0cd8b235917a2cff64438d6686372d3c63193ad3
Amritpal Nagra <amritpal.nagra@sifars.com>
2023-06-06 16:00:19 +0530

    add transition method to statemachine

--- statemachine.js
+++ statemachine.js
@@ -12,4 +12,14 @@ class StateMachine {
     const allowedStates = this.transitions[currentState] || [];
     return allowedStates.includes(nextState);
   }
+
+  transition(currentState, nextState) {
+    if (this.isValidTransition(currentState, nextState)) {
+      console.log(`Transition from ${currentState} to ${nextState}`);
+      return nextState;
+    } else {
+      console.log(`Invalid transition from ${currentState} to ${nextState}`);
+      return currentState;
+    }
+  }
 }
